5299bd19cab9c95d4610fb37ed9a7a5f9058b22b
Ken Stipek <ken@brivity.com>
2014-11-30 14:50:47 -0800

    Add leaderboard with tests and linechart with test.

--- lib/footpad.rb
+++ lib/footpad.rb
@@ -19,13 +19,56 @@ module Footpad
         else
           result[:item] = [item]
         end
+      else
+        result[type] = options[type] unless options[type].nil?
       end
-      result[type] = options[type] unless options[type].nil?
     end
 
     result.to_json
   end
   alias_method :nss, :number_and_secondary_stat
+
+
+  def leaderboard(primary_value, options = {})
+    result = Hash.new
+    values = Array.new
+
+    primary_value.each do |t|
+      if t[2].nil?
+        values << {
+          label: t[0],
+          value: t[1]
+        }
+      else
+        values << {
+          label: t[0],
+          value: t[1],
+          previous_rank: t[2]
+        }
+      end
+    end
+
+    result[:format] = options[:format] if options.has_key?(:format)
+    result[:unit] = options[:unit] if options.has_key?(:unit)
+    result[:items] = values
+
+    result.to_json
+  end
+  alias_method :lb, :leaderboard
+
+
+  def linechart(primary_value, options = {})
+    result = Hash.new
+
+    result[:item] = primary_value
+    result[:settings] = Hash.new
+
+    options.each do |key, value|
+      result[:settings][key] = value
+    end
+
+    result.to_json
+  end
 
 
 
--- test/test_footpad.rb
+++ test/test_footpad.rb
@@ -32,4 +32,19 @@ class FootpadTest < Minitest::Test
     test_data = '{"item":[{"value":274057,"text":"Past 7 days"},["38594","39957","35316","35913","36668","45660","41949"]]}'
     assert_equal test_data, number_and_secondary_stat(274057, text: 'Past 7 days', trendline: ["38594","39957","35316","35913","36668","45660","41949"])
   end
+
+  def test_leaderboard
+    test_data = '{"items":[{"label":"Peter","value":234,"previous_rank":2},{"label":"Patrick","value":232,"previous_rank":1},{"label":"Jon","value":230},{"label":"Clara","value":220},{"label":"Tom","value":215,"previous_rank":7},{"label":"Sylvester","value":200},{"label":"David","value":195,"previous_rank":5},{"label":"Matt","value":190},{"label":"William","value":185},{"label":"Rose","value":182,"previous_rank":11},{"label":"Colin","value":178,"previous_rank":10},{"label":"Paul","value":172},{"label":"Amy","value":166},{"label":"Christopher","value":162}]}'
+    assert_equal test_data, leaderboard([['Peter',234,2],['Patrick',232,1],['Jon',230],['Clara',220],['Tom',215,7],['Sylvester',200,],['David',195,5],['Matt',190],['William',185],['Rose',182,11],['Colin',178,10],['Paul',172],['Amy',166],['Christopher',162]])
+  end
+
+  def test_leaderboard_with_format
+    test_data = '{"format":"currency","unit":"USD","items":[{"label":"Peter","value":234,"previous_rank":2},{"label":"Patrick","value":232,"previous_rank":1},{"label":"Jon","value":230},{"label":"Clara","value":220},{"label":"Tom","value":215,"previous_rank":7},{"label":"Sylvester","value":200},{"label":"David","value":195,"previous_rank":5},{"label":"Matt","value":190},{"label":"William","value":185},{"label":"Rose","value":182,"previous_rank":11},{"label":"Colin","value":178,"previous_rank":10},{"label":"Paul","value":172},{"label":"Amy","value":166},{"label":"Christopher","value":162}]}'
+    assert_equal test_data, leaderboard([['Peter',234,2],['Patrick',232,1],['Jon',230],['Clara',220],['Tom',215,7],['Sylvester',200,],['David',195,5],['Matt',190],['William',185],['Rose',182,11],['Colin',178,10],['Paul',172],['Amy',166],['Christopher',162]], format: 'currency', unit: 'USD')
+  end
+
+  def test_linechart
+    test_data = '{"item":[10,20,30,5,5,1,25,15],"settings":{"axisx":["02/02","02/03","02/04","02/05","02/06","02/07","02/08","02/09"],"axisy":[],"color":"#ff00aa"}}'
+    assert_equal test_data, linechart([10,20,30,5,5,1,25,15], axisx: (2..9).map {|i| "02/0#{i}"}, axisy: [], color: '#ff00aa')
+  end
 end
